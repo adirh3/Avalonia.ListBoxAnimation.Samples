d83852edd429f8a7a461bf7020bc7c855393510a
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2023-01-26 20:09:50 +0800

    make it work somehow

--- Avalonia.ListBoxAnimation.Samples/SelectingItemsControlExtension.cs
+++ Avalonia.ListBoxAnimation.Samples/SelectingItemsControlExtension.cs
@@ -5,8 +5,7 @@ using Avalonia.Animation.Easings;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Templates;
-using Avalonia.Rendering.Composition;
-using Avalonia.Rendering.Composition.Animations;
+using Avalonia.Rendering.Composition; 
 using Avalonia.VisualTree;
 
 namespace Avalonia.ListBoxAnimation.Samples;
@@ -24,16 +23,15 @@ public class SelectingItemsControlExtension
 
     private static void OnEnableSelectionAnimation(Control control, AvaloniaPropertyChangedEventArgs args)
     {
-        if (control is SelectingItemsControl listBox)
+        if (control is not SelectingItemsControl listBox) return;
+        
+        if (args.NewValue is true)
         {
-            if (args.NewValue is true)
-            {
-                listBox.PropertyChanged += SelectingItemsControlPropertyChanged;
-            }
-            else
-            {
-                listBox.PropertyChanged -= SelectingItemsControlPropertyChanged;
-            }
+            listBox.PropertyChanged += SelectingItemsControlPropertyChanged;
+        }
+        else
+        {
+            listBox.PropertyChanged -= SelectingItemsControlPropertyChanged;
         }
     }
 
@@ -60,7 +58,7 @@ public class SelectingItemsControlExtension
         StartOffsetAnimation(newSelection, oldSelection);
     }
 
-    private static async void StartOffsetAnimation(TemplatedControl nextSelection, TemplatedControl prevSelection)
+    private static void StartOffsetAnimation(TemplatedControl nextSelection, TemplatedControl prevSelection)
     {
         // Find the indicator border
         if (prevSelection.GetTemplateChildren().FirstOrDefault(s => s.Name == "PART_SelectedPipe") is not Visual
@@ -68,7 +66,6 @@ public class SelectingItemsControlExtension
             nextSelection.GetTemplateChildren().FirstOrDefault(s => s.Name == "PART_SelectedPipe") is not Visual
                 nextInd)
         {
-            Console.WriteLine("Weird");
             return;
         }
 
@@ -81,118 +78,74 @@ public class SelectingItemsControlExtension
             tmpNextPos is not { } nextPos ||
             tmpPrevPos is not { } prevPos)
         {
-            Console.WriteLine("WTF");
             return;
         }
 
-        ;
-
         var isVertical = deltaPos is { X: 0 };
-        var dist = (float)deltaPos.X + (float)deltaPos.Y;
-
-        float outgoingEndPosition, incomingStartPosition;
-
-        if (isVertical)
-        {
-            outgoingEndPosition = (float)(nextPos.Y - prevPos.Y);
-            incomingStartPosition = (float)(prevPos.Y - nextPos.Y);
-        }
-        else
-        {
-            outgoingEndPosition = (float)(nextPos.X - prevPos.X);
-            incomingStartPosition = (float)(prevPos.X - nextPos.X);
-        }
-
-        Console.WriteLine($"f {outgoingEndPosition} {incomingStartPosition}");
-
-        var visual = ElementComposition.GetElementVisual(nextInd);
-        if (visual == null) return;
-        var comp = visual.Compositor;
-
-        await comp.RequestCommitAsync();
+        var dist = (float)Math.Abs(deltaPos.X + (float)deltaPos.Y);
         var prevSize = prevInd.Bounds.Size;
         var nextSize = nextInd.Bounds.Size;
 
-        float dir = ((isVertical ? nextPos.Y : nextPos.X) > (isVertical ? prevPos.Y : prevPos.X)) ? -1 : 1;
+        var dir = (isVertical ? nextPos.Y : nextPos.X) > (isVertical ? prevPos.Y : prevPos.X);
 
-        // Play the animation on both the previous and next indicators
-        PlayIndicatorAnimations(prevInd, isVertical, 
-            0,
-            0, prevSize, nextSize,
-            true);
+        ResetIndicator(prevInd);
 
-
-        PlayIndicatorAnimations(nextInd, isVertical,
-            dir * Math.Abs(dist),
-            0, prevSize, nextSize,
-            false);
+        PlayIndicatorAnimations(nextInd, isVertical, dir, dist, prevSize, nextSize);
     }
 
-
-    private static void PlayIndicatorAnimations(Visual? indicator, bool isVertical, float from, float to,
-        Size beginSize, Size endSize,
-        bool isOutgoing)
+    private static void ResetIndicator(Visual? indicator, float opacity = 0)
     {
         if (indicator == null) return;
         var visual = ElementComposition.GetElementVisual(indicator);
         if (visual == null) return;
+        visual.Opacity = opacity;
+    }
+
+
+    private static void PlayIndicatorAnimations(Visual? indicator, bool isVertical, bool isForward, float distance,
+        Size beginSize, Size endSize)
+    {
+        if (indicator == null) return;
+        var visual = ElementComposition.GetElementVisual(indicator);
+        if (visual == null) return;
+        
         var comp = visual.Compositor;
-
-        Console.WriteLine($"O: {visual.Offset}");
-        Console.WriteLine($"S: {visual.Scale}");
-        Console.WriteLine($"C: {visual.CenterPoint}");
-        var duration = TimeSpan.FromSeconds(5);
+        var duration = TimeSpan.FromSeconds(0.6);
         var size = indicator.Bounds.Size;
-        var dimension = isVertical ? size.Height : size.Width;
+        var dimension = (float)(isVertical ? size.Height : size.Width);
 
-        double beginScale, endScale;
+        float beginScale, endScale;
 
         if (isVertical)
         {
-            beginScale = beginSize.Height / size.Height;
-            endScale = endSize.Height / size.Height;
+            beginScale = (float)(beginSize.Height / size.Height);
+            endScale = (float)(endSize.Height / size.Height);
         }
         else
         {
-            beginScale = beginSize.Width / size.Width;
-            endScale = endSize.Width / size.Width;
+            beginScale = (float)(beginSize.Width / size.Width);
+            endScale = (float)(endSize.Width / size.Width);
         }
 
 
         var singleStep = new StepEasing();
         var compositionAnimationGroup = comp.CreateAnimationGroup();
 
-        if (isOutgoing)
-        {
-            // fade the outgoing indicator so it looks nice when animating over the scroll area
-            var opacityAnim = comp.CreateScalarKeyFrameAnimation();
-            opacityAnim.InsertKeyFrame(0.0f, 1.0f);
-            opacityAnim.InsertKeyFrame(0.333f, 1.0f);
-            opacityAnim.InsertKeyFrame(1.0f, 0.0f);
-            opacityAnim.Duration = duration;
-            opacityAnim.Target = "Opacity";
-            compositionAnimationGroup.Add(opacityAnim);
-        }
 
         Vector3 ScalarModifier(Vector3 reference, float scalar = 1f) =>
             isVertical ? reference with { Y = scalar } : reference with { X = scalar };
 
         var scaleAnim = comp.CreateVector3KeyFrameAnimation();
-        var s1 = (float)beginScale;
-        var s2 = (float)((Math.Abs(to - from) / dimension) + (from < to ? endScale : beginScale));
-        var s3 = (float)endScale;
+        var s2 = Math.Abs(distance) / dimension + (isForward ? endScale : beginScale);
 
-        Console.WriteLine($"s {s1} - {s2} - {s3}");
-
-        scaleAnim.InsertKeyFrame(0.0f, ScalarModifier(visual.Scale, s1));
-        scaleAnim.InsertKeyFrame(0.333f, ScalarModifier(visual.Scale, s2));
-        scaleAnim.InsertKeyFrame(1.0f, ScalarModifier(visual.Scale, s3));
+        scaleAnim.InsertKeyFrame(0f, ScalarModifier(visual.Scale, s2));
+        scaleAnim.InsertKeyFrame(1.0f, ScalarModifier(visual.Scale, endScale), new CircularEaseInOut());
         scaleAnim.Duration = duration;
         scaleAnim.Target = "Scale";
 
         var centerAnim = comp.CreateVector3KeyFrameAnimation();
-        var c1 = (float)(from < to ? 0.0f : dimension);
-        var c2 = (float)(from < to ? dimension : 0.0f);
+        var c1 = isForward ? 0.0f : dimension;
+        var c2 = isForward ? dimension : 0.0f;
 
         Console.WriteLine($"c {c1} - {c2}");
 
@@ -200,8 +153,7 @@ public class SelectingItemsControlExtension
         centerAnim.InsertKeyFrame(1.0f, ScalarModifier(visual.CenterPoint, c2), singleStep);
         centerAnim.Duration = duration;
         centerAnim.Target = "CenterPoint";
-
-
+        
         compositionAnimationGroup.Add(scaleAnim);
         compositionAnimationGroup.Add(centerAnim);
         visual.StartAnimationGroup(compositionAnimationGroup);
@@ -212,7 +164,7 @@ public class SelectingItemsControlExtension
     {
         public double Ease(double progress)
         {
-            return (Math.Abs(progress - 1) < double.Epsilon) ? 1d : 0d;
+            return Math.Abs(progress - 1) < double.Epsilon ? 1d : 0d;
         }
     }
 
